cf745cf964bee9670c08327ed52b346a295b4704
altor <arthur.d-azemar@inria.fr>
2021-02-09 15:07:45 +0000

    add apt configuration to install script

--- install.sh
+++ install.sh
@@ -2,13 +2,17 @@
 
 WORK_DIR=/root
 
-apt update
-apt upgrade
+# configure `apt` to use bionic repository only for PowerAPI specific packages
+cp bionic.list /etc/apt/sources.list.d/
+cp powerapi.pref /etc/apt/preferences.d/
 
-# install linux-kernel
+apt update
+apt upgrade -y
+
+# install linux kernel `4.18.0-25`
 apt install -y linux-image-4.18.0-25-generic linux-modules-4.18.0-25-generic
 
-# patched LibPFM build
+# build and install a patched version of `libpfm`
 apt install -y build-essential git devscripts debhelper dpatch python3-dev libncurses-dev swig
 update-alternatives --install /usr/bin/python python /usr/bin/python3 1
 git clone -b msr-pmu-old https://github.com/gfieni/libpfm4.git $WORK_DIR/libpfm4
@@ -17,7 +21,7 @@ fakeroot debian/rules binary
 dpkg -i ../libpfm4_10.1_amd64.deb
 dpkg -i ../libpfm4-dev_10.1_amd64.deb
 
-# sensor build
+# build and install `hwpc-sensor`
 git clone https://github.com/powerapi-ng/hwpc-sensor.git $WORK_DIR/hwpc-sensor
 apt install -y clang-tidy cmake pkg-config libczmq4 libczmq-dev libsystemd-dev uuid-dev libmongoc-1.0-0
 cd $WORK_DIR/hwpc-sensor
@@ -25,4 +29,4 @@ mkdir build
 cd build
 cmake -DCMAKE_BUILD_TYPE="${Release}" -DWITH_MONGODB="${ON}" ..
 make -j $(getconf _NPROCESSORS_ONLN)
-
+mv hwpc-sensor /usr/bin/
