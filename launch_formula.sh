6ab3bc21d2d00f2402a0ad67dea0457dd8f4c383
altor <arthur.d-azemar@inria.fr>
2021-04-12 14:13:38 +0000

    delete unused files

--- launch_formula.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cgcreate -g perf_event:formula
-echo $$ >/sys/fs/cgroup/perf_event/formula/cgroup.procs
-
-python3.8 -m smartwatts $@
-
-cgdelete perf_event:formula
--- set_cgroup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-daemon=$(pidof sshd named irqbalance mdadm polkitd atd systemd-logind cron rsyslogd dbus-daemon systemd-timesyncd systemd-udevd lvmetad systemd-journald)
-cgdelete perf_event:daemon
-
-cgcreate -g perf_event:daemon
-
-cgclassify -g perf_event:daemon $daemon
